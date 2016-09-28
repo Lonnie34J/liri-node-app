593f9e102f36402d5cffb7bbf041b0c1bc1e02fa
Lonnie Jones <lonnie34j@gmail.com>
2016-09-28 01:15:33 -0400

    fixed functions

--- liri.js
+++ liri.js
@@ -6,26 +6,6 @@ var liriCommands = process.argv[2];
 var userInput = process.argv[3];
 
 
-
-function activateCommands(){
-	switch(liriCommands){
-		case "my-tweets":
-		//getTweets();
-		break;
-		//When user inputs song they need to include a + between words(look into fixing this if there is extra time)
-		case "spotify-this-song " + userInput:
-		getSpotify();
-		break;
-
-		case "movie-this " + userInput:
-		//getMovie();
-		break;
-};
-
-};
-
-
-
 //grabs and creates variable for all twitter related content
 	var twitterRequest = require("twitter");
 	var keys = require("./keys");
@@ -34,11 +14,34 @@ function activateCommands(){
 	//console.log(keys);
 	//=========================================================//
 
+var request = require("request");
 
+
+	switch(liriCommands){
+		case "my-tweets":
+			getTweets(myTweets);
+			break;
+		//When user inputs song they need to include a + between words(look into fixing this if there is extra time)
+		case "spotify-this-song " + userInput:
+			getSpotify();
+			break;
+
+		case "movie-this " + userInput:
+			getMovie();
+			break;
+};
+
+
+
+
+
+
+
+function getTweets(myTweets){
 	//sets the search parameters to a varible 
 	var params = {screen_name : 'Odd_Realm', count : 20};
 
-		myTweets.get('statuses/user_timeline', params, function getTweets (error, data, response) {
+		myTweets.get('statuses/user_timeline', params, function (error, data, response) {
 	  	if (!error) {
 	  		//console.log(data[2].created_at);
 	  		//console.log(data[2].text);
@@ -55,10 +58,12 @@ function activateCommands(){
 	  	}
 	});
 
-
+}
 //Spotify code 
 
-	spotify.search({ type: 'track', query: userInput }, function getSpotify (err, data) {
+function getSpotify(){
+	
+	spotify.search({ type: 'track', query: userInput }, function (err, data) {
 	    if ( err ) {
 	        console.log('Error occurred: ' + err);
 	        return;
@@ -80,26 +85,27 @@ function activateCommands(){
 	 
 	   
 	});
-
+};
 
 //omdb code ================================================
 
-var request = require("request");
+function getMovie() {
 
-request('http://www.omdbapi.com/?t=' +userInput+ '&y=2016&plot=short&tomatoes=true&r=json', function getMovie(error, response, body){
+	request('http://www.omdbapi.com/?t=' +userInput+ '&y=2016&plot=short&tomatoes=true&r=json', function (error, response, body){
 
-	if (!error && response.statusCode == 200){
-		console.log("Movie Title: "+JSON.parse(body)["Title"]);
-		console.log("Release Year: "+JSON.parse(body)["Year"]);
-		console.log("IMDB Rating: "+JSON.parse(body)["imdbRating"]);
-		console.log("Country: "+JSON.parse(body)["Country"]);
-		console.log("Language: "+JSON.parse(body)["Language"]);
-		console.log("Plot: "+JSON.parse(body)["Plot"]);
-		console.log("Actors: "+JSON.parse(body)["Actors"]);
-		console.log("Rotten Tomatoes Rating: "+JSON.parse(body)["tomatoRating"]);
-		console.log("Rotten Tomatoes Url: "+JSON.parse(body)["tomatoURL"]);
-	}else{
-		throw error;
-	}
+		if (!error && response.statusCode == 200){
+			console.log("Movie Title: "+JSON.parse(body)["Title"]);
+			console.log("Release Year: "+JSON.parse(body)["Year"]);
+			console.log("IMDB Rating: "+JSON.parse(body)["imdbRating"]);
+			console.log("Country: "+JSON.parse(body)["Country"]);
+			console.log("Language: "+JSON.parse(body)["Language"]);
+			console.log("Plot: "+JSON.parse(body)["Plot"]);
+			console.log("Actors: "+JSON.parse(body)["Actors"]);
+			console.log("Rotten Tomatoes Rating: "+JSON.parse(body)["tomatoRating"]);
+			console.log("Rotten Tomatoes Url: "+JSON.parse(body)["tomatoURL"]);
+		}else{
+			throw error;
+		}
 
-});
+	});
+}
